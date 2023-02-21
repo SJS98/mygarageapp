cd135ca47962e404ac9dc10f9d587d433fd2404d
SJS98 <107642013+SJS98@users.noreply.github.com>
2023-02-21 11:24:49 +0530

    goodpull1

---
+++ 1_Home/script.js
@@ -0,0 +1,14 @@
+
+function change(x){
+  if(x==1){  
+    document.getElementById("iframe").src = "";
+  }
+  else if(x==2){
+    document.getElementById("iframe").src = "../2_Order/order.html";
+  }
+  else if(x==3){
+    document.getElementById("iframe").src = "../3_Bill/Bill.html";
+  }
+  else if(x==4)
+    document.getElementById("iframe").src = "";
+}
