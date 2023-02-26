920efeab228265024e7ad22f7b55dfe84a19eeec
SJS98 <107642013+SJS98@users.noreply.github.com>
2023-02-26 20:33:47 +0530

    JScreateElement1

--- 3_Bill/billscript.js
+++ 3_Bill/billscript.js
@@ -1,15 +1,7 @@
 let total = 0 ;
 
-
 function add(x){
-  let val = document.getElementById("price"+x).value ;
-  total += parseInt(val) ; 
-  document.getElementById("val").innerHTML = total ;
-  count(x) ;
-}
-
-const count = (cn) => {
-  let val = document.getElementById("c"+cn).textContent ;
-  let c1 = parseInt(val) ;
-  document.getElementById("c"+cn).innerHTML = c1+1 ;
+  const btn = document.createElement("div");
+  btn.setAttribute("class","mcard");
+  document.getElementById("cart").appendChild(btn);
 }
